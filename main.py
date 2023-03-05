76310e9a7a93cf904b734a79b6fb92fb6c9748bf
Jacqueline Romero <jaky.romerohernandez@live.com.mx>
2023-03-04 19:29:10 -0600

    Tipifica datos, archivo guardado

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'guerrero':
         return (f'{char_name} causó {5 + randint(3, 5)} de daño al enemigo')
     if char_class == 'mago':
         return (f'{char_name} causó {5 + randint(5, 10)} de daño al enemigo')
     if char_class == 'sanador':
         return (f'{char_name} causó {5 + randint(-3, -1)} de daño al enemigo')
-    return None
 
 
-def defense(char_name, char_class):
+def defense(char_name: str, char_class: str) -> str:
     if char_class == 'guerrero':
         return (f'{char_name} bloqueó {10 + randint(5, 10)} de daño')
     if char_class == 'mago':
@@ -21,7 +20,7 @@ def defense(char_name, char_class):
     return None
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'guerrero':
         return (f'{char_name} usó una habilidad especial "Aguante {80 + 25}"')
     if char_class == 'mago':
@@ -31,7 +30,7 @@ def special(char_name, char_class):
     return None
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, eres un Guerrero,'
               'experto en combate'
@@ -46,7 +45,7 @@ def start_training(char_name, char_class):
           'defender, para bloquear un ataque enemigo,'
           'o especial, para usar tu poder especial.')
     print('Si no quieres entrenar, presiona saltar (skip).')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Introduce un comando: ')
         if cmd == 'ataque':
@@ -58,9 +57,9 @@ def start_training(char_name, char_class):
     return 'El entrenamiento ha terminado.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = None
+    char_class: str = None
     while approve_choice != 'y':
         char_class = input('Introduce la clase de tu personajes: Guerrero,'
                            'guerrero; Mago, mago; Sanador, sanador: ')
@@ -79,17 +78,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
+def main() -> None:
     print('¡Saludos, aventurero!')
     print('Antes de comenzar a jugar...')
-    char_name = input('... indica tu nombre: ')
+    char_name: str = input('... indica tu nombre: ')
     print(f'¡Bienvenido, {char_name}! '
           'Tienes 80 puntos de aguante, 5 puntos'
           'de ataque, y 10 puntos de defensa.')
     print('Puedes elegir una de las tres maneras de la Fuerza:')
     print('Guerrero, Mago, Sanador')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
+  
+main ()
